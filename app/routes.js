1ff6c941bac5c1fa357939bb6dde38103a7c5030
changlewojing <linxiaoming@gmail.com>
2018-11-10 18:35:27 +0800

    session登录后打印

--- app/routes.js
+++ app/routes.js
@@ -26,6 +26,7 @@ module.exports = function(app, passport) {
 		}),
         function(req, res) {
             console.log("hello");
+	    console.log(req.session);
 
             if (req.body.remember) {
               req.session.cookie.maxAge = 1000 * 60 * 3;
@@ -57,6 +58,8 @@ module.exports = function(app, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
+console.log("this is /profie");
+console.log(req.session);
 		res.render('profile.ejs', {
 			user : req.user // get the user out of session and pass to template
 		});
